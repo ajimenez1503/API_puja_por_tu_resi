c0f1fc1ed70112bce0e7e6904b761aed4241a258
softwarejimenez <softwarejimenezz@gmail.com>
2017-02-05 19:35:14 +0100

    college update password and email

--- src/AppBundle/Controller/ProfileCollegeController.php
+++ src/AppBundle/Controller/ProfileCollegeController.php
@@ -67,4 +67,87 @@ class ProfileCollegeController extends Controller
 
 
 
+        /**
+         * @ApiDoc(
+         *  description="This method update the password of a user (College). Can be called by user (College).",
+         *  requirements={
+         *      {
+         *          "name"="old_password",
+         *          "dataType"="String",
+         *          "description"="Old password of the user"
+         *      },
+         *      {
+         *          "name"="new_password",
+         *          "dataType"="String",
+         *          "description"="New Password of the user"
+         *      },
+         *  },
+         * )
+         */
+        public function updatePasswordAction(Request $request)
+        {
+            $sizePassword=$this->container->getParameter('sizePassword');
+            $old_password=$request->request->get('old_password');
+            $new_password=$request->request->get('new_password');
+            if (!$this->get('app.validate')->validateLenghtInput($this->get('validator'),$old_password,$sizePassword,$sizePassword) or !$this->get('app.validate')->validateLenghtInput($this->get('validator'),$new_password,$sizePassword,$sizePassword)){
+                return $this->returnjson(false,'Contraseña invalidad. Longitud '.$sizePassword.' caracteres.');
+            }
+            if(!$this->get('app.validate')->validateOldPassword($this->get('validator'),$old_password)){
+                return $this->returnjson(false,'La contraseña actual no es correcta.');
+            }
+            try {
+                $user=$this->get('security.token_storage')->getToken()->getUser();
+                $encoder = $this->container->get('security.password_encoder');
+                $new_passwor_encoded = $encoder->encodePassword($user, $new_password);
+
+                $user->setPassword($new_passwor_encoded);
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($user);
+
+                //Doctrine looks through all of the objects that it's managing to see if they need to be persisted to the database.
+                $em->flush();
+            } catch (\Exception $pdo_ex) {
+                return $this->returnjson(false,'SQL exception.');
+            }
+            return $this->returnjson(true,'La contraseña se ha cambiado correctamente.');
+        }
+
+
+        /**
+         * @ApiDoc(
+         *  description="This method update the email of a user (College). Can be called by user (College).",
+         *  requirements={
+         *      {
+         *          "name"="email",
+         *          "dataType"="String",
+         *          "description"="New email of the user"
+         *      },
+         *  },
+         * )
+         */
+        public function updateEmailAction(Request $request)
+        {
+            $email=$request->request->get('email');
+
+            if (!$this->get('app.validate')->validateEmail($this->get('validator'),$email) ){
+                return $this->returnjson(false,'El email no es valido.');
+            }
+            try {
+                $user=$this->get('security.token_storage')->getToken()->getUser();
+                $user->setEmail($email);
+                $em = $this->getDoctrine()->getManager();
+                // tells Doctrine you want to (eventually) save the Product (no queries is done)
+                $em->persist($user);
+
+                //Doctrine looks through all of the objects that it's managing to see if they need to be persisted to the database.
+                $em->flush();
+            } catch (\Exception $pdo_ex) {
+                return $this->returnjson(false,'SQL exception.');
+            }
+            return $this->returnjson(true,'El email se ha cambiado correctamente.');
+        }
+
+
+
 }

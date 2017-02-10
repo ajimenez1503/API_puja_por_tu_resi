bd80a1c8ed40cb5b7de123b2079c548f5507341d
softwarejimenez <softwarejimenezz@gmail.com>
2017-02-10 17:36:03 +0100

    get all info (agreement, room, rents, student_profile) of 1 or several students

--- src/AppBundle/Controller/ProfileCollegeController.php
+++ src/AppBundle/Controller/ProfileCollegeController.php
@@ -375,4 +375,94 @@ class ProfileCollegeController extends Controller
 
 
 
+        /**
+         * @ApiDoc(
+         *  description="Get all the student of a college. For every student get: list_rents, room, agreement, student_data. Can be called by user (College ).",
+         * )
+         */
+        public function getStudentsCompleteAction()
+        {
+
+            $user=$this->get('security.token_storage')->getToken()->getUser();
+            if ($user->getRoles()[0]=="ROLE_COLLEGE"){
+                $student=$user->getStudents();
+                $output=array();
+                for ($i = 0; $i < count($student); $i++) {
+                    $rents_student=array();
+                    $rents=$student[$i]->getRents()->getValues();
+                    for ($j = 0; $j < count($rents); $j++) {
+                        array_push($rents_student,$rents[$j]->getJSON());
+                    }
+                    array_push($output,
+                        array(
+                            "student"=>$student[$i]->getJSON(),
+                            "room"=>$student[$i]-> getCurrentAgreement()->getRoom()->getJSON(),
+                            "agreement"=>$student[$i]-> getCurrentAgreement()->getJSON(),
+                            "rents"=>$rents_student,
+                        )
+                    );
+                }
+                return $this->returnjson(true,'Informacion completa de todos los estudiantes.',$output);
+            }else{
+                return $this->returnjson(false,'Esta function solo puede ser llamada por el rol RESIDENCIA');
+            }
+        }
+
+
+
+        /**
+         * @ApiDoc(
+         *  description="Get student information get: list_rents, room, agreement, student_data. Can be called by user (College ).",
+         *  requirements={
+         *      {
+         *          "name"="username_student",
+         *          "dataType"="String",
+         *          "description"="Username of the student  "
+         *      },
+         *  },
+         * )
+         */
+        public function getStudentCompleteAction($username_student)
+        {
+            $user=$this->get('security.token_storage')->getToken()->getUser();
+            if ($user->getRoles()[0]=="ROLE_COLLEGE"){//college
+                //validate $username
+                if (is_null($username_student) || !$this->get('app.validate')->validateLenghtInput($this->get('validator'),$username_student,9,9)){
+                        return $this->returnjson(False,'Username '.$username_student.' no es valido.');
+                }
+                $student = $this->getDoctrine()->getRepository('AppBundle:Student')->find($username_student);
+                if (!$student) {
+                    return $this->returnjson(False,'estudiante con username '.$username_student.' no existe.');
+                }
+                //verify signed agreement and college
+                $agreement=$student->getCurrentAgreement();
+                if($agreement){
+                    if($agreement->verifyAgreementSigned()){
+                        if ($agreement->getCollege()==$user){
+                            $rents_student=array();
+                            $rents=$student->getRents()->getValues();
+                            for ($j = 0; $j < count($rents); $j++) {
+                                array_push($rents_student,$rents[$j]->getJSON());
+                            }
+                            $output=array(
+                                "student"=>$student->getJSON(),
+                                "room"=>$student-> getCurrentAgreement()->getRoom()->getJSON(),
+                                "agreement"=>$student-> getCurrentAgreement()->getJSON(),
+                                "rents"=>$rents_student,
+                            );
+                        }else{
+                            return $this->returnjson(false,'El estudiante '.$student->getUsername().' tiene un contrato pero no con esta residencia '.$user->getUsername().'');
+                        }
+                    }else{
+                        return $this->returnjson(false,'El estudiante '.$student->getUsername().' tiene un contrato pero sin firmar.');
+                    }
+                }else{
+                    return $this->returnjson(false,'El estudiante '.$student->getUsername().' no tiene contrato con ninguna residencai.');
+                }
+                return $this->returnjson(true,'Informacion completa del estudiente.',$output);
+            }else{
+                return $this->returnjson(False,'ROLE unknow');
+            }
+        }
+
 }

ec5069b80bf77a06def83ea2b51be8434870cbda
Antonio Jimenez <softwarejimenezz@gmail.com>
2017-02-22 19:29:44 +0100

    bash add contrab job and fix phpmyadmin

--- dependencies.sh
+++ dependencies.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo apt-get update && sudo apt-get upgrade
 ############################################################
-#install python 
+#install python
 ############################################################
 
-sudo add-apt-repository ppa:fkrull/deadsnakes -y 
+sudo add-apt-repository ppa:fkrull/deadsnakes -y
 sudo apt-get update
 sudo apt-get install python3.5
 
@@ -12,16 +12,22 @@ sudo apt-get install python3.5
 #install LAMP
 ############################################################
 #Install Apache
-sudo apt-get install apache2 -y 
+sudo apt-get install apache2 -y
 
 # Install MySQL
-sudo apt-get install mysql-server -y 
-sudo apt-get install phpmyadmin -y 
+sudo apt-get install mysql-server -y
+
+#Install phpmyadmin
+sudo apt-get install phpmyadmin -y
+#gedit /etc/apache2/apache2.conf
+#add:
+  # #phpMyAdmin Configuration
+  # Include /etc/phpmyadmin/apache.conf
 
 #Install PHP
 sudo apt-get install php7.0 php-pear libapache2-mod-php7.0 php7.0-mysql -y
 
-#restart server 
+#restart server
 sudo /etc/init.d/apache2 restart
 
 ############################################################
@@ -32,7 +38,7 @@ cd Desktop/
 git clone https://github.com/softwarejimenez/API_puja_por_tu_resi.git
 cd API_puja_por_tu_resi/
 
-#install composer 
+#install composer
 sudo apt install composer -y
 composer install
 
@@ -47,15 +53,11 @@ php bin/console server:run
 ############################################################
 #CRONTAB automatically job
 ############################################################
-crontab -e
-job: 0 23 * * *  python3.5 /home/jimenez/Desktop/API_puja_por_tu_resi/periodical_request/assigne_room.py >> /home/jimenez/Desktop/API_puja_por_tu_resi/periodical_request/output_log.txt
-
+crontab -l | { cat; echo "0 23 * * *  python3.5 /home/jimenez/Desktop/API_puja_por_tu_resi/periodical_request/assigne_room.py >> /home/jimenez/Desktop/API_puja_por_tu_resi/periodical_request/output_log.txt"; } | crontab -
 ############################################################
 #DEPLOYMENT CLIENT
 ############################################################
 cd /var/www/html/
 sudo git clone https://github.com/softwarejimenez/web_puja_por_tu_resi.git
 
-#now you can open the web :http://127.0.0.1/web_puja_por_tu_resi/#!/welcome 
-
-
+#now you can open the web :http://127.0.0.1/web_puja_por_tu_resi/#!/welcome
